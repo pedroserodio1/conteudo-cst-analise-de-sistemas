9067161133b4a0acf1d9e8362b45d2540bea787d
Pedro Serôdio <serodiomg@gmail.com>
2022-08-04 13:08:43 -0300

    aula3 sem elif

---
+++ LÓGICA DE PROGRAMAÇÃO E ALGORITMOS/Aula3/exercicioNota.py
@@ -0,0 +1,8 @@
+nota1 = float(input('Digite a nota da 1º materia: '))
+nota2 = float(input('Digite a nota da 2º materia: '))
+nota3 = float(input('Digite a nota da 3º materia: '))
+
+if nota1>=7 and nota2>=7 and nota3>=7:
+    print('Passou de ano')
+else:
+    print('Não passou de ano')
